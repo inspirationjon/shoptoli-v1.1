c5f26e1e3d04616294925b1a87e5320f083622af
ilhomjon55 <ilhomshamidinov55@gmail.com>
2021-05-14 19:17:12 +0500

    1

--- src/screens/Products/Products.js
+++ src/screens/Products/Products.js
@@ -1,9 +1,32 @@
-import ProductBox from '../../components/ProductBox/ProductBox'
+import { NavLink, Route, Switch } from 'react-router-dom'
 import './Products.scss'
 function Products() {
     return (
         <section className='products-section'>
-            <ProductBox />
+            <ul className='products-section__nav'>
+                <li className='products-section__nav-item'>
+                    <NavLink
+                        className='products-section__nav-link'
+                        to='/products'
+                        exact>
+                        Categories
+                    </NavLink>
+                </li>
+                <li className='products-section__nav-item'>
+                    <NavLink
+                        className='products-section__nav-link'
+                        to='/categories'
+                        exact>
+                        Products
+                    </NavLink>
+                </li>
+            </ul>
+
+            <div className='products-section__content'>
+                <Switch>
+                    <Route path='/products' />
+                </Switch>
+            </div>
         </section>
     )
 }
